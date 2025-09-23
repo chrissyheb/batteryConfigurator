e8adac00eb062efd94e6ffee9e1c88f4daa7cf9a
Christopher Pfindel <christopher.pfindel@neoom.com>
2025-09-23 07:33:44 +0200

    chore: remove comments

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,26 +4,6 @@ import { loadLocal, saveLocal } from '@/utils/storage';
 import { validate, getInitialConfig } from '@/spec/builder';
 import { buildErrorIndex } from '@/utils/errors';
 
-// const defaultCfg: any = {
-//   Customer: '',
-//   ModularPlc: { Version: '0.0.3', HardwareVariant: 'Terra' },
-//   Units: {
-//     Ems: {
-//       Equipment: [
-//       ]
-//     },
-//     Main: {
-//       Type: 'Blokk',
-//       Equipment: [
-//         { Name: 'Smartmeter', DisplayName: 'Local Power Measurement', Type: 'SmartmeterMain', Hardware: 'Virtual', Guid: uuid() },
-//         { Name: 'Kaco 1 + BMS 1 - Inverter1 / Battery1', Type: 'BatteryInverter',
-//           Inverter: { Name: 'Kaco1', Type: 'InverterKaco', Guid: uuid(), Config: { InverterType: 'Kaco', NominalInverterPower: '92000' } },
-//           Battery: { Name: 'Battery1', Type: 'BatteryPylontechM1xBms', Guid: uuid(), Config: { BatteryType: 'PylontechM1C', BatteryCabinetCount: '1', BatteryCabinetModuleCount: '24' } } }
-//       ]
-//     }
-//   }
-// };
-
 type Action = { type: 'SET'; payload: any } | { type: 'PATCH'; payload: Partial<any> };
 
 function reducer(state: any, action: Action): any
@@ -39,7 +19,6 @@ export function useStore()
     const local = loadLocal();
     if (local) { return local; }
     return getInitialConfig();
-    //return defaultCfg;
   });
 
   const result = useMemo(() => { return validate(state); }, [state]);
