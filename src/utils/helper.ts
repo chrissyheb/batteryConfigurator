7ea81ae46b75c0364098ebdbe66cb448c90288b3
Christopher Pfindel <christopher.pfindel@neoom.com>
2025-09-24 18:20:14 +0200

    feat (Configurator) Add validation implementation for new types (number, numberWithUnit)

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,5 @@
+import { IndexStringType } from '@/spec/catalog';
+
 export function addUnit(n: number, unit: string): string
 {
   const s:string = n.toString() + unit;
@@ -22,3 +24,23 @@ export function stripUnit(s: string): [number, string]
     return [num, unit];
   }
 }
+
+
+export const indexStringToString = (e: IndexStringType[]): string[] =>
+{
+  let output:string[] = [];
+  let i = 0;
+  e.forEach(element => {
+    if (element[0] !== undefined && element[1] !== undefined)
+    {
+      output.push(element[0] + " - " + element[1]);
+      i++;
+    }
+  });
+  return output;
+};
+export const stringToIndexString = (v: string): IndexStringType =>
+{
+  const parts = v.split(" - ");
+  return [Number(parts[0]), parts[1]] as IndexStringType;
+};
