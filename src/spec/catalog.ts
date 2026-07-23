0e05cd6c35f74513f47cba97301ae8b57e35d3de
Christopher Pfindel <christopher.pfindel@neoom.com>
2026-07-23 10:30:07 +0200

    feat (Configurator) NT-19278 Add Smartmeter Phoenix MA370

--- src/spec/catalog.ts
+++ src/spec/catalog.ts
@@ -93,8 +93,8 @@ export const enums = {
   ems: {
     smartmeterHardwareToTypes: {
       CarloGavazzi: ['EM24'],
-      Phoenix: ['EM375'],
       Janitza: ['UMG 96 PA', 'UMG 96 RM', 'UMG 509 Pro', 'UMG 801'],
+      Phoenix: ['EM375', 'MA370'],
       Custom: ['Custom'],
       Beckhoff: ['El34x3'],
       Virtual: ['Virtual']
